a672950fb1ec29bb1888f31f0d2427c3c016c71b
Chumachenkovlad <chumachenko.vla@gmail.com>
2017-06-09 14:49:00 +0300

    upgrade logic

--- README.md
+++ README.md
@@ -2,12 +2,24 @@
 
 ### Dependencies
 
-> $ pip3 install biopython
+> $ pip3 install biopython 
+
+or you can download windows package from http://biopython.org/wiki/Download
 
 ## HOWTO
 
-1. Paste at _organisms_ file "taxonomy name" (search query) 
+1. Paste at **organisms_<name_of_project>.txt** file "taxonomy name" (search query) 
 2. At **main.py** find variable **MAM_GIDS** where add yours protein **Name** and **GID**
 3. Run script:
 
-> $ python3 main.py
+> $ python3 main.py project=<name_of_project>
+
+> $ python3 main.py project=gastro (example)
+
+
+**<name_of_project>** min length 3 chars
+
+Script will save alignments at directory saved_data_**<name_of_project>**
+Script will save results at directory **results** with name **<name_of_project>.tsv**
+
+You can open .tsv file at MS EXCEL or another program for edit received data
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 import os
 import time
+import sys
 
 """
 qblast(program, database, sequence, url_base='https://blast.ncbi.nlm.nih.gov/Blast.cgi', auto_format=None, composition_based_statistics=None, db_genetic_code=None, endpoints=None, entrez_query='(none)', expect=10.0, filter=None, gapcosts=None, genetic_code=None, hitlist_size=50, i_thresh=None, layout=None, lcase_mask=None, matrix_name=None, nucl_penalty=None, nucl_reward=None, other_advanced=None, perc_ident=None, phi_pattern=None, query_file=None, query_believe_defline=None, query_from=None, query_to=None, searchsp_eff=None, service=None, threshold=None, ungapped_alignment=None, word_size=None, alignments=500, alignment_view=None, descriptions=500, entrez_links_new_window=None, expect_low=None, expect_high=None, format_entrez_query=None, format_object=None, format_type='XML', ncbi_gi=None, results_file=None, show_overview=None, megablast=None)
@@ -31,9 +32,32 @@ INDATA = {
     }
 }
 part = 'skin'
-DIR_SAVED = INDATA[part]['dir_saved']
-RESULT_FILE_NAME = INDATA[part]['result_file_name']
-ORGANISMS_FILE = INDATA[part]['orgaism_file']
+if len(sys.argv) < 2:
+    print('Error. Any project name. try > python3 main.py project=<chosen_name>')
+    sys.exit(1)
+
+project = sys.argv[1]
+project = project.split('=')
+
+if len(project) != 2:
+    print('Value error. try > python3 main.py project=<chosen_name>')
+    sys.exit(1)
+
+if project[0] != 'project':
+    print('Key error. try > python3 main.py project=<chosen_name>')
+    sys.exit(1)
+
+if len(project[1]) < 3:
+    print('Value length error. try > python3 main.py project=<chosen_name>. Use more then 2 chars')
+    sys.exit(1)
+
+project = project[1]
+
+DIR_SAVED = 'saved_data_{}'.format(project)
+RESULT_FILE_NAME = str(project)
+ORGANISMS_FILE = 'organisms_{}.txt'.format(project)
+RESULTS_DIR = 'results'
+
 CONDITIONS = {
     'identities': 25,
     'e-value': 0.0001,
@@ -77,9 +101,7 @@ alignments = []
 
 def get_goal_organisms():
     with open(ORGANISMS_FILE) as organisms_source:
-        # print(organisms_source.read().split('\n'))
         return organisms_source.read().split('\n')
-        # return goalOrganisms
 
 
 def get_data(gid, goal):
@@ -98,6 +120,8 @@ def get_data(gid, goal):
 
 
 def save_data(data, filename):
+    if os.path.exists(DIR_SAVED):
+        os.makedirs(DIR_SAVED)
     print('Start saving data to file {}.xml'.format(filename))
     with open("{}/{}.xml".format(DIR_SAVED, filename), 'w') as savedFile:
         savedFile.write(data.read())
@@ -157,7 +181,6 @@ def parse_data(mam, goal, filename):
                 })
 
             parsedData[mam].append(sort_list_of_alignments(alignment_seqs))
-            # parsedData[mam].append(alignment_seqs)
     openfile.close()
 
 
@@ -194,7 +217,6 @@ def get_path_to_file(filename):
 
 def make_mams_aligns(goal):
     for mam in MAM_GIDS:
-
         print('Start parsing data {} {}'.format(mam, goal))
         filename = get_filename(mam, goal)
 
@@ -219,109 +241,103 @@ def string_to_line(string):
     return False
 
 
+def make_mam_table(data):
+    alignment = {
+        "organism_name": '',
+        "MamA": {'statistic': '-', 'table_value': '-'},
+        "MamB": {'statistic': '-', 'table_value': '-'},
+        "MamM": {'statistic': '-', 'table_value': '-'},
+        "MamO": {'statistic': '-', 'table_value': '-'},
+        "MamE": {'statistic': '-', 'table_value': '-'},
+        "MamN": {'statistic': '-', 'table_value': '-'},
+        "MamK": {'statistic': '-', 'table_value': '-'},
+        "MamH": {'statistic': '-', 'table_value': '-'},
+    }
+    for organism in all_unique_organisms:
+        alg = alignment.copy()
+        alg['organism'] = organism
+        for mam in data:
+            for cur_alg in data[mam]:
+                for data_alg in cur_alg:
+                    if data_alg['organism'] == organism:
+                        alg[mam] = {
+                            'statistic': data_alg['statistic'],
+                            'table_value': '{} ({}%) {}'.format(data_alg['e-value'], data_alg['identities'], data_alg['protein'])
+                        }
+        alignments.append(alg)
+
+
+def old_print_alignment(a):
+    table_string = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+        a["organism"], a["MamA"], a["MamB"], a["MamM"], a["MamO"], a["MamE"], a["MamN"], a["MamK"], a["MamH"])
+    return table_string
+
+
+def get_statistic_and_production_types(a):
+    i = 0
+    production_type, statistic = False, False
+    if '-' not in [a['MamB']['statistic'], a['MamM']['statistic'], a['MamO']['statistic'], a['MamE']['statistic']]:
+        production_type = 'A'
+        if all(item['statistic'] == 'H' for item in [a['MamB'], a['MamM'], a['MamO'], a['MamE']]):
+            stats = 'H'
+        else:
+            stats = 'DZ'
+    else:
+        return False, False
+    if a['MamA']['statistic'] != '-':
+        production_type = 'C'
+        if a['MamA']['statistic'] == 'H' and not stats == 'DZ':
+            stats = 'H'
+        else:
+            stats = 'DZ'
+    if a['MamK']['statistic'] != '-' and a['MamA']['statistic'] != '-':
+        production_type = 'CC'
+        if all(item == 'H' for item in [a['MamK']['statistic'], a['MamA']['statistic']]) and not stats == 'DZ':
+            stats = 'H'
+        else:
+            stats = 'DZ'
+    return production_type, stats
+
+
+def print_alignment(a):
+
+    table_aligns = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t'.format(
+        a["MamA"]['table_value'],
+        a["MamB"]['table_value'],
+        a["MamM"]['table_value'],
+        a["MamO"]['table_value'],
+        a["MamE"]['table_value'],
+        a["MamN"]['table_value'],
+        a["MamK"]['table_value'],
+        a["MamH"]['table_value'])
+
+    production_type, statistic = get_statistic_and_production_types(a)
+    if not production_type and not statistic:
+        return False
+    table_string = '{}\t{}\t{}\t{}'.format(a["organism"], production_type, statistic, table_aligns)
+    print(table_string)
+    return table_string
+
+
 def save_final_alignments(data):
-    with open('{}.txt'.format(RESULT_FILE_NAME), 'w') as outfile:
-        # for mam in data:
-        #     outfile.write(string_to_line(mam))
-        #     for item in data[mam]:
-        #         line = '\t{}\t{}\t{} ({}%)'.format(
-        #             item['organism'],
-        #             item['protein'],
-        #             item['e-value'],
-        #             item['identities'])
-        #         outfile.write(string_to_line(line))
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    with open('{}/{}.tsv'.format(RESULTS_DIR, RESULT_FILE_NAME), 'w') as outfile:
+        title = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+            'Taxonomy', 'Production type', 'Statistic class',
+            "MamA", "MamB", "MamM", "MamO", "MamE", "MamN", "MamK", "MamH")
+        print(title)
+        outfile.write(string_to_line(title))
         for alg in data:
             algn_string = string_to_line(print_alignment(alg))
             if bool(algn_string):
                 outfile.write(algn_string)
 
 
-def make_mam_table(data):
-
-    alignment = {
-        "organism_name": '',
-        # "statistic": '',
-        "MamA": '-',
-        "MamB": '-',
-        "MamM": '-',
-        "MamO": '-',
-        "MamE": '-',
-        "MamN": '-',
-        "MamK": '-',
-        "MamH": '-',
-    }
-    for organism in all_unique_organisms:
-        alg = alignment.copy()
-        alg['organism'] = organism
-        # alignments.append(alg)
-        for mam in data:
-            for cur_alg in data[mam]:
-                for data_alg in cur_alg:
-                    if data_alg['organism'] == organism:
-                        # alg['statistic'] = data_alg['statistic']
-                        alg[mam] = data_alg['statistic'] #'{} ({}%) {}'.format(data_alg['e-value'], data_alg['identities'], data_alg['protein'])
-        alignments.append(alg)
-
-
-def old_print_alignment(a):
-    table_string = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
-        a["organism"],
-        a["MamA"],
-        a["MamB"],
-        a["MamM"],
-        a["MamO"],
-        a["MamE"],
-        a["MamN"],
-        a["MamK"],
-        a["MamH"])
-    print(table_string)
-    return table_string
-
-
-def print_alignment(a):
-    # table_string = '{}\t\t\t{}\t{}\t{}\t{}\t'.format(
-    #     a["organism"],
-    #     # a["MamA"],
-    #     a["MamB"],
-    #     a["MamM"],
-    #     a["MamO"],
-    #     a["MamE"],
-    #     # a["MamN"],
-    #     # a["MamK"],
-    #     # a["MamH"]
-    # )
-    production_type = False
-    statistic = False
-    if not [a['MamB'], a['MamM'], a['MamO'], a['MamE']].__contains__('-'):
-        production_type = 'A'
-        if all(item == 'H' for item in [a['MamB'], a['MamM'], a['MamO'], a['MamE']]):
-            statistic = 'H'
-        else:
-            statistic = 'DZ'
-    else:
-        return False
-    if a['MamA'] != '-':
-        production_type = 'C'
-        if a['MamA'] == 'H' and not statistic == 'DZ':
-            statistic = 'H'
-        else:
-            statistic = 'DZ'
-    if a['MamK'] != '-' and a['MamA'] != '-':
-        production_type = 'CC'
-        if all(item == 'H' for item in [a['MamK'], a['MamA']]) and not statistic == 'DZ':
-            statistic = 'H'
-        else:
-            statistic = 'DZ'
-    table_string = '{}\t{}\t{}'.format(a["organism"], production_type, statistic)
-    print(table_string)
-    return table_string
-
-
 def main():
     goal_organisms = get_goal_organisms()
     for goal in goal_organisms:
-        print('\n{}/{}\n' \
-            .format(
+        print('\n{}/{}\n'.format(
                 goal_organisms.index(goal) + 1,
                 len(goal_organisms)))
         make_mams_aligns(goal)
@@ -332,7 +348,5 @@ if __name__ == '__main__':
     for organism in all_organisms:
         all_unique_organisms.add(organism)
     make_mam_table(parsedData)
-
-    # print_parsed_data(parsedData)
     save_final_alignments(alignments)
-    # get_goal_organisms()
+
